36365279326f5c6e8641fe44d24c858eaa538d8d
杨锋 <yangfeng@jianzhikeji.com>
2026-07-07 16:47:09 +0800

    fix: prevent middleware crash when Clerk env vars not configured

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+function hasClerkKeys() {
+  return Boolean(
+    process.env.CLERK_SECRET_KEY && process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+  );
+}
+
+export default hasClerkKeys()
+  ? clerkMiddleware()
+  : () => NextResponse.next();
 
 export const config = {
   matcher: [
